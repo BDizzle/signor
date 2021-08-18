1ef8321e61664b1527db71381b78b4e6492f5e03
Brandon McKenzie <brandon@zeplay.tv>
2021-08-18 14:53:50 -0500

    got signs of life from signing library

--- src/app.py
+++ src/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, json, request, abort
+from aws_requests_auth.aws_auth import AWSRequestsAuth
+from requests import Request
 
 companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
 
@@ -17,7 +19,22 @@ def validate_request():
   elif request.form['SESSION_TOKEN'] == None:
     abort(400, 'missing SESSION_TOKEN')
   else:
-    return json.dumps(companies)
+    return sign_request(request.form['ACCESS_KEY'], request.form['SECRET_ACCESS_KEY'])
+
+def sign_request(access_key, secret_key):
+    auth = AWSRequestsAuth( aws_access_key=request.form['ACCESS_KEY'],
+                            aws_secret_access_key=request.form['SECRET_ACCESS_KEY'],
+                            aws_host='foo',
+                            aws_region='bar',
+                            aws_service='baz'
+                            )
+    req = Request('GET', 'https://url.com', auth=auth)
+    prepared = req.prepare();
+
+    print(prepared.headers)
+
+    return json.dumps(dict(prepared.headers));
+
 
 if __name__ == '__main__':
     app.run() 
